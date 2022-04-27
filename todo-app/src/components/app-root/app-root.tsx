e54c8bf90bd65a05439f1d4dd98f9b341a077213
David Kennedy <4w55g7@masqt.com>
2022-04-27 09:27:55 -0600

    Remove boilerplate components

--- todo-app/src/components/app-root/app-root.tsx
+++ todo-app/src/components/app-root/app-root.tsx
@@ -8,20 +8,12 @@ import { Component, h } from '@stencil/core';
 export class AppRoot {
   render() {
     return (
-      <div>
-        <header>
-          <h1>Stencil App Starter</h1>
-        </header>
-
-        <main>
-          <stencil-router>
-            <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url="/" component="app-home" exact={true} />
-              <stencil-route url="/profile/:name" component="app-profile" />
-            </stencil-route-switch>
-          </stencil-router>
-        </main>
-      </div>
+      <stencil-router>
+        <stencil-route-switch scrollTopOffset={0}>
+          <stencil-route url="/" component="todo-list" exact={true} />
+        </stencil-route-switch>
+      </stencil-router>
     );
   }
 }
+
